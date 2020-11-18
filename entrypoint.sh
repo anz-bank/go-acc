ecdcc04c6b6352555465d1f1018eb395698010f4
joshcarp <joshuacarpeggiani@gmail.com>
2020-11-19 09:39:33 +1100

    Replace TAGS with FLAGS

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,10 +3,10 @@ set -e
 
 if [ -z "$INPUT_IGNORE" ]; then
   # generate coverage profile
-  /go/bin/go-acc ./... -o=cover.out -- ${TAGS}
+  /go/bin/go-acc ./... -o=cover.out -- ${FLAGS}
 else
   # generate coverage profile (with ignores)
-  /go/bin/go-acc ./... --ignore "${INPUT_IGNORE//[[:space:]]/}" -o=cover.out -- ${TAGS}
+  /go/bin/go-acc ./... --ignore "${INPUT_IGNORE//[[:space:]]/}" -o=cover.out -- ${FLAGS}
 fi
 
 go tool cover -func=cover.out | go run /main.go
