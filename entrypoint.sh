3d4b24d5cb1ab10d22587e404d69a1d2855fa3b3
joshcarp <joshuacarpeggiani@gmail.com>
2020-11-19 10:09:42 +1100

    Use INPUT_FLAGS for entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,10 +3,10 @@ set -e
 
 if [ -z "$INPUT_IGNORE" ]; then
   # generate coverage profile
-  /go/bin/go-acc ./... -o=cover.out -- ${FLAGS}
+  /go/bin/go-acc ./... -o=cover.out -- ${INPUT_FLAGS}
 else
   # generate coverage profile (with ignores)
-  /go/bin/go-acc ./... --ignore "${INPUT_IGNORE//[[:space:]]/}" -o=cover.out -- ${FLAGS}
+  /go/bin/go-acc ./... --ignore "${INPUT_IGNORE//[[:space:]]/}" -o=cover.out -- ${INPUT_FLAGS}
 fi
 
 go tool cover -func=cover.out | go run /main.go
